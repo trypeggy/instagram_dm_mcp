43ae7b40a8387dedc8e4dbb2a0785528a7487b41
Jake Bloom <gday@jakebloom.me>
2025-06-22 14:32:29 -0700

    Added more primitives

--- src/mcp_server.py
+++ src/mcp_server.py
@@ -1,6 +1,8 @@
 from fastmcp import FastMCP
 from instagrapi import Client
 import argparse
+from typing import Optional
+
 
 INSTRUCTIONS = """
 This server is used to send messages to a user on Instagram.
@@ -21,6 +23,26 @@ def send_instagram_dm(username: str, message: str):
    user_id = client.user_id_from_username(username)
    client.direct_send(message, [user_id])
    return "Message sent to user."
+
+
+@mcp_server.tool(
+   description="Get a list of message threads from the user's Instagram account.",
+)
+def get_threads(
+   amount: int = 20,
+   selected_filter: str = "",
+   thread_message_limit: Optional[int] = None,
+):
+   threads = client.direct_threads(amount, selected_filter, thread_message_limit)
+   return threads
+
+
+@mcp_server.tool(
+   description="Get a list of messages from a thread.",
+)
+def get_messages(thread_id: str, amount: int = 20):
+   messages = client.direct_messages(thread_id, amount)
+   return messages
 
 
 if __name__ == "__main__":
